9752fde1e55e0bcd8cb7289d3fe621d7ed3c8be8
Rayhanul Islam Shuvo <rayhanshuvo.bd@gmail.com>
2024-02-29 22:13:45 +0600

    correct entity
    correct entity

--- src/main/java/com/ibcs/demo/entity/BankAccount.java
+++ src/main/java/com/ibcs/demo/entity/BankAccount.java
@@ -4,85 +4,25 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BankAccount {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String accountType;
     private String accountName;
-    private String accountNumber;
-    private String currentBalance;
+    private Double accountNumber;
+    private Double currentBalance;
     private String bankName;
     private String branchName;
 
-    public BankAccount() {
-    }
-
-    public BankAccount(Long id, String accountType, String accountName, String accountNumber, String currentBalance, String bankName, String branchName) {
-        this.id = id;
-        this.accountType = accountType;
-        this.accountName = accountName;
-        this.accountNumber = accountNumber;
-        this.currentBalance = currentBalance;
-        this.bankName = bankName;
-        this.branchName = branchName;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getAccountType() {
-        return accountType;
-    }
-
-    public void setAccountType(String accountType) {
-        this.accountType = accountType;
-    }
-
-    public String getAccountName() {
-        return accountName;
-    }
-
-    public void setAccountName(String accountName) {
-        this.accountName = accountName;
-    }
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public void setAccountNumber(String accountNumber) {
-        this.accountNumber = accountNumber;
-    }
-
-    public String getCurrentBalance() {
-        return currentBalance;
-    }
-
-    public void setCurrentBalance(String currentBalance) {
-        this.currentBalance = currentBalance;
-    }
-
-    public String getBankName() {
-        return bankName;
-    }
-
-    public void setBankName(String bankName) {
-        this.bankName = bankName;
-    }
-
-    public String getBranchName() {
-        return branchName;
-    }
-
-    public void setBranchName(String branchName) {
-        this.branchName = branchName;
-    }
 }
--- src/main/java/com/ibcs/demo/entity/Employee.java
+++ src/main/java/com/ibcs/demo/entity/Employee.java
@@ -2,8 +2,16 @@ package com.ibcs.demo.entity;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,72 +25,5 @@ public class Employee {
     @OneToOne
     private BankAccount bankAccountNumber;
 
-    public Employee(Long id, String employeeId, String name, String address, String mobile, Grade grade, BankAccount bankAccountNumber) {
-        this.id = id;
-        this.employeeId = employeeId;
-        this.name = name;
-        this.address = address;
-        this.mobile = mobile;
-        this.grade = grade;
-        this.bankAccountNumber = bankAccountNumber;
-    }
 
-    public Employee() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
-
-    public BankAccount getBankAccountNumber() {
-        return bankAccountNumber;
-    }
-
-    public void setBankAccountNumber(BankAccount bankAccountNumber) {
-        this.bankAccountNumber = bankAccountNumber;
-    }
 }
